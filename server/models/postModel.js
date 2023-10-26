4a2e1ed6c551b29d07557c398920e6aad8c4a743
[punyakon.p] <Punyakon857@gmail.com>
2023-10-26 15:40:50 +0700

    update post schema

--- server/models/postModel.js
+++ server/models/postModel.js
@@ -1,5 +1,39 @@
 const mongoose = require("mongoose");
 
-const postSchema = mongoose.Schema({});
+const postSchema = mongoose.Schema({
+  postId: {
+    type: Number,
+    require: true,
+    unique: true,
+  },
+  postTitle: {
+    type: String,
+    require: true,
+  },
+  postDesciption: {
+    type: String,
+    require: true,
+  },
+  postImage: {
+    type: String,
+    require: true,
+  },
+  authorUid: {
+    type: String,
+    require: true,
+  },
+  authorName: {
+    type: String,
+    require: true,
+  },
+  authorEmail: {
+    type: String,
+    require: true,
+  },
+  authorProfile: {
+    type: String,
+    require: true,
+  },
+});
 
 module.exports = mongoose.model("Post", postSchema);
