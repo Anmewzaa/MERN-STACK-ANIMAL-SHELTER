3c69015b87d9fe8769d9d5de41cce3dcc3f384b3
[punyakon.p] <Punyakon857@gmail.com>
2023-11-04 23:27:00 +0700

    update image preview function

--- client/src/pages/AddPostPage.tsx
+++ client/src/pages/AddPostPage.tsx
@@ -1,10 +1,37 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 // Components
 import Navbar from "../components/Navbar";
 
+import { auth } from "../firebase/firebase-config";
+
 const AddPostPage = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!auth.currentUser) return navigate("/");
+  }, []);
+  const [images, setImages] = useState([]);
+  const [imagesURLs, setImagesURLs] = useState([]);
+  useEffect(() => {
+    if (images.length < 1) return;
+    const newImageUrls = [];
+    images.forEach((image) => newImageUrls.push(URL.createObjectURL(image)));
+    setImagesURLs(newImageUrls);
+  }, [images]);
+
+  const onImageChange = (e) => {
+    setImages([...e.target.files]);
+  };
+
   return (
     <>
       <Navbar />
+      <div className="mx-auto max-w-[960px]">
+        <input type="file" accept="image/*" onChange={onImageChange} />
+        {imagesURLs.map((imagesrc) => (
+          <img src={imagesrc} />
+        ))}
+      </div>
     </>
   );
 };
