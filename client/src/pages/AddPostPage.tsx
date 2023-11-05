8602001a26720a7a98cde093e41c3acca1feb6ff
[punyakon.p] <Punyakon857@gmail.com>
2023-11-05 23:40:48 +0700

    Fix Create Post

--- client/src/pages/AddPostPage.tsx
+++ client/src/pages/AddPostPage.tsx
@@ -9,37 +9,41 @@ import "react-quill/dist/quill.snow.css";
 import Navbar from "../components/Navbar";
 
 const AddPostPage = () => {
+  const API_URL = import.meta.env.VITE_API_URL;
   const navigate = useNavigate();
   useEffect(() => {
     if (!auth.currentUser) return navigate("/");
   }, []);
   const [images, setImages] = useState([]);
-  const [imagesURLs, setImagesURLs] = useState([]);
-  useEffect(() => {
-    if (images.length < 1) return;
-    const newImageUrls = [];
-    images.forEach((image) => newImageUrls.push(URL.createObjectURL(image)));
-    setImagesURLs(newImageUrls);
-  }, [images]);
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const onImageChange = (e) => {
-    setImages([...e.target.files]);
+    setImages(e.target.files[0]);
   };
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (!(title && images.length > 0 && description))
-      return alert("input required");
+
+    const form = new FormData();
+    form.append("postTitle", title);
+    form.append("postDesciption", description);
+    form.append("postImage", images[0]);
+    form.append("authorUid", auth.currentUser?.uid);
+    form.append("authorName", auth.currentUser?.displayName);
+    form.append("authorEmail", auth.currentUser?.email);
+    form.append("authorProfile", auth.currentUser?.photoURL);
+
+    axios
+      .post(`${API_URL}/create`, form)
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
   };
 
   return (
     <>
       <Navbar />
       <div className="mx-auto max-w-[960px]">
-        <form onSubmit={handleSubmit}>
-          {imagesURLs.map((imagesrc) => (
-            <img src={imagesrc} width={300} className="mb-4" />
-          ))}
+        <form onSubmit={handleSubmit} encType="multipart/form-data">
+          {JSON.stringify(images)}
           <div className="mb-4">
             <label className="label">
               <span className="label-text">Image</span>
@@ -49,6 +53,7 @@ const AddPostPage = () => {
               accept="image/*"
               className="block my-2 file-input file-input-bordered w-full max-w-xs"
               onChange={onImageChange}
+              required
             />
           </div>
           <div className="mb-4">
@@ -62,6 +67,7 @@ const AddPostPage = () => {
                 className="input input-bordered w-full max-w-xs"
                 value={title}
                 onChange={(e) => setTitle(e.target.value)}
+                required
               />
             </div>
           </div>
